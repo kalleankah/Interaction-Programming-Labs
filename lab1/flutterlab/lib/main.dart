0dadadafbedd065786deffa795678397d70be76c
Karl Eknefelt <k.eknefelt@gmail.com>
2019-12-03 14:37:37 +0100

    lab 1 in flutter done

---
+++ lab1/flutterlab/lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home: Scaffold(
+    appBar: AppBar(
+      title: Text('Example 1'),
+    ),
+    body: ListView(
+        children: <Widget>[
+          Image.network(
+            'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg',
+          ),
+          Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                FlatButton(
+                    onPressed: () => AlertDialog(title: Text('This button does nothing')),
+                    child: Text('Button'),
+                    color: Colors.grey
+                ),
+                FlatButton(
+                    onPressed: () => AlertDialog(title: Text('This button does nothing')),
+                    child: Text('Button'),
+                    color: Colors.grey
+                )
+              ]
+          ),
+          Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                FlatButton(
+                    onPressed: () => AlertDialog(title: Text('This button does nothing')),
+                    child: Text('Button'),
+                    color: Colors.grey
+                ),
+                FlatButton(
+                    onPressed: () => AlertDialog(title: Text('This button does nothing')),
+                    child: Text('Button'),
+                    color: Colors.grey
+                )
+              ]
+          ),
+          Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Text('Email: '),
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'name@example.com'
+                    ),
+                  )
+                )
+              ]
+          )
+        ]
+      )
+    )
+));
