804a7299d2fadb45ed25ca4e514f976fbc68dc78
Karl Eknefelt <k.eknefelt@gmail.com>
2020-01-11 00:16:52 +0100

    gradle properties

--- lab2/lib/main.dart
+++ lab2/lib/main.dart
@@ -1,60 +1,22 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 void main() => runApp(MaterialApp(
-  debugShowCheckedModeBanner: false,
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text('Credit Card test'),
-    ),
-    body:
-    ListView(
-      padding: const EdgeInsets.all(8),
-      children: <Widget>[
-        CardWidget()
-      ],
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(
+        appBar: AppBar(
+          title: Text('Credit Card test'),
+        ),
+        body:
+        ListView(
+          padding: const EdgeInsets.all(12),
+          children: <Widget>[
+            MyCustomForm()
+          ],
+        )
     )
-  )
 ));
-
-//This is the credit card widget
-class CardWidget extends StatefulWidget {
-  @override
-  _CardWidgetState createState() => _CardWidgetState();
-}
-
-//The credit card's state class which handles events
-class _CardWidgetState extends State<CardWidget> {
-  bool _cardBackground = true;
-  final myController = TextEditingController();
-
-  @override
-  Widget build(BuildContext context) {
-    SystemChrome.setPreferredOrientations([
-    DeviceOrientation.portraitUp
-    ]);
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Image.asset(
-          (_cardBackground ? 'assets/images/1.jpeg' : 'assets/images/2.jpeg')
-        ),
-        TextField(
-          controller: myController,
-        ),
-        MyCustomForm()
-      ]
-    );
-  }
-
-//  Delete text controller
-  @override
-  void dispose() {
-    // Clean up the controller when the widget is disposed.
-    myController.dispose();
-    super.dispose();
-  }
-}
 
 // This is the Form widget.
 class MyCustomForm extends StatefulWidget {
@@ -68,46 +30,107 @@ class MyCustomForm extends StatefulWidget {
 class MyCustomFormState extends State<MyCustomForm> {
   // Global key that uniquely identifies the Form widget
   final _formKey = GlobalKey<FormState>();
+//  Controller that copies text from form to the card
+  final myController = TextEditingController();
+//  Control which background image is used for the card
+  bool _cardBackground = true;
 
+//  Build the app
   @override
   Widget build(BuildContext context) {
-    // Builds a Form widget using the _formKey
-    return Form(
-        key: _formKey,
-        child: Column(
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.all(20),
-              child: TextFormField(
-                onChanged: ,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Card Number'
-                ),
-                // The validator receives the text that the user has entered.
-                validator: (value) {
-                  if (value.isEmpty) {
-                    return 'Please enter some text';
-                  }
-                  return null;
-                },
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp
+    ]);
+    return Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          AspectRatio(
+              aspectRatio: 675 / 435,
+              child: Stack(
+                children: <Widget>[
+                  ClipRRect(
+                      borderRadius: BorderRadius.circular(20),
+                      child: Image.asset(
+                          (_cardBackground ? 'assets/images/2.jpeg' : 'assets/images/1.jpeg')
+                      )
+                  ),
+                  Center(
+                    widthFactor: 1.0,
+                    child: FractionallySizedBox(
+                        heightFactor: 0.2,
+                        widthFactor: 1.0,
+                        child: Container(
+                            decoration: BoxDecoration(
+                              color: Color(0x80505050),
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            child: Padding(
+                              padding: EdgeInsets.all(6),
+                              child: Text(
+                                  myController.text,
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 24,
+                                  )
+                              ),
+                            )
+                        )
+                    ),
+                  )
+                ],
               )
-            ),
-            RaisedButton(
-              color: Colors.blue,
-              onPressed: () {
-                // Validate returns true if the form is valid, otherwise false.
-                if (_formKey.currentState.validate()) {
-                  Scaffold.of(context).showSnackBar(SnackBar(content: Text('Processing Data')));
-                }
-              },
-              child: Text(
-                'Submit',
-                style: TextStyle(color: Colors.white),
-              ),
-            )
-          ]
-        )
+          ),
+          Form(
+              key: _formKey,
+              child: Column(
+                  children: <Widget>[
+                    Padding(
+                        padding: EdgeInsets.all(20),
+                        child: TextFormField(
+//                          onChanged: ,
+                          controller: myController,
+                          keyboardType: TextInputType.number,
+                          decoration: InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: 'Card Number'
+                          ),
+                          // The validator receives the text that the user has entered.
+                          validator: (value) {
+                            if (value.isEmpty) {
+                              return 'Please enter some text';
+                            }
+                            return null;
+                          },
+                        )
+                    ),
+                    RaisedButton(
+                      color: Colors.blue,
+                      onPressed: () {
+                        // Validate returns true if the form is valid, otherwise false.
+                        if (_formKey.currentState.validate()) {
+                          Scaffold.of(context)
+                              .showSnackBar(SnackBar(content: Text('Processing Data')));
+                        }
+                      },
+                      child: Text(
+                        'Submit',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    )
+                  ]
+              )
+          )
+        ]
     );
+  }
+
+//  Dispose controller
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is removed from the
+    // widget tree.
+    myController.dispose();
+    super.dispose();
   }
 }
